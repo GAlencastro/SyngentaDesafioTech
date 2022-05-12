2bf9ce2757345dae30a88e4a9967558b4340a3cd
GAlencastro <38190063+GAlencastro@users.noreply.github.com>
2022-05-12 16:07:44 -0300

    challenge-python-hotel-reservation-test
    Exercise tested and done. 
    
    For testing: pytest -v -s

--- src/my_module.py
+++ src/my_module.py
@@ -1,78 +1,69 @@
 import re
-import sys
-from __init__ import Hotel
+import numpy as np
+from collections import OrderedDict
 
-hotels = ['Lakewood', 'Bridgewood', 'Ridgewood']
+# Dictionary with information about prices according to both types of guests, rating and names
+HOTELS_INFO = {
+    'names': ['Lakewood', 'Bridgewood', 'Ridgewood'],
+    'rates': [3, 4, 5],
+    'daily_values': {
+        'business_days': {
+            'regular': np.array([110.0, 160.0, 220.0]),
+            'rewards': np.array([80.0, 110.0, 100.0]),
+        },
+        'weekend_days': {
+            'regular': np.array([90.0, 60.0, 150.0]),
+            'rewards': np.array([80.0, 50.0, 40.0]),
+        },
 
-#Função para tirar info do input
-def FilterLine(line):
-    infosHospedagem = []
-    aux = line.split(':')
-    clienteTipo = aux[0]
+    }
+}
 
-    infosHospedagem.append(clienteTipo)
-    aux = aux[1]
 
-    r = re.compile(r'\bmon\b | \btues\b | \bwed\b | \bthur\b | \bfri\b | \bsat\b | \bsun\b' , flags=re.I | re.X)
-    tmp = r.findall(aux)
-
-    infosHospedagem.append(tmp)
-
-    return infosHospedagem
-
-def Empate(lista_Empate):
-    aux = lista_Empate[0]['Classificacao']
-    maiorClass = lista_Empate[0]
-
-    for i in range(len(lista_Empate)):
-        aux = maiorClass
-        if(lista_Empate[i]['Classificacao'] > aux):
-            maiorClass = lista_Empate[i]
-            aux = maiorClass
-    return maiorClass['Nome']
-
-def HotelMaisBarato(hotels, vetorInput):
-    lista_Empate = []
-    aux = hotels[0].totalValue(vetorInput)
-    lista_Empate.append(aux)
-    tmp = aux['ValorTotal']
-    hotelresultado = aux['Nome']
-    for i in range(len(hotels)):
-        aux2 = hotels[i].totalValue(vetorInput)
-        if(tmp >= aux2['ValorTotal']):
-                if (tmp == aux2['ValorTotal']):
-                    lista_Empate.append(aux2)
-                tmp = aux2['ValorTotal']
-                hotelresultado = aux2['Nome']
-
-    if(len(lista_Empate) != 1):
-            hotelresultado = Empate(lista_Empate)
-    
-    if(hotelresultado == 'Lakewood'):
-        return 0
-    if(hotelresultado == 'Bridgewood'):
-        return 1
-    else:
-        return 2
-
-def get_cheapest_hotel(number):
-    cheapest_hotel = hotels[number]
+# Main function to test prices and return the cheapest hotel during the given days
+def get_cheapest_hotel(inputString: str) -> str:
+    info_reservation = filter_line(inputString)
+    cheapest_hotel = evaluate_minimum_price(**get_infos(info_reservation))
+    print(cheapest_hotel)
     return cheapest_hotel
 
 
-def main (argv):
-    if (len(argv) != 2):
-        sys.exit("Numero Errado de argumentos")
-    
-    arq = open(str(argv[1]), 'r')
+# Function to evaluate all the prices and return lowest one. In case of a tie, return the hotel with more rating
+# Returns name of the cheapest hotel
+def evaluate_minimum_price(
+        num_business_days: int,
+        num_weekend_days: int,
+        guest_type: str,
+) -> str:
+    prices = num_business_days * HOTELS_INFO['daily_values']['business_days'][guest_type] + \
+             num_weekend_days * HOTELS_INFO['daily_values']['weekend_days'][guest_type]
 
-    line = arq.readline()
+    best_hotels_idx = np.where(prices == np.min(prices))
+    rates_masked = np.array(HOTELS_INFO['rates'], dtype=int)[best_hotels_idx]
+    hotels_masked = np.array(HOTELS_INFO['names'])[best_hotels_idx]
 
-    arqFiltro = FilterLine(line)
-    resp = HotelMaisBarato(hotels, arqFiltro)
+    return hotels_masked[np.argmax(rates_masked)]
 
-    cheapest_hotel = get_cheapest_hotel(resp)
 
-    arq.close()
+# Function for filtering the input string about the guest, getting the type of guest,
+# number of days and their types (business or weekend days).
+# Returns list with first index always informing type of guest
+def filter_line(line: str) -> list:
+    r = re.compile(r'\bRegular\b | \bRewards\b | \bmon\b | \btues\b | \bwed\b |'
+                   r' \bthur\b | \bfri\b | \bsat\b | \bsun\b',
+                   flags=re.I | re.X)
+    tmp = np.array(r.findall(line))
+    info_reservation = list(np.char.lower(tmp))
+    return info_reservation
 
-    print(cheapest_hotel)
+
+# Function to consolidate the information about the guest, counting how many days each day type has.
+# Returns a dictionary with guest type, number of each day type
+def get_infos(line_info: list[str]) -> dict:
+    infos = OrderedDict()
+    infos['guest_type'] = line_info[0]
+
+    infos['num_weekend_days'] = line_info.count('sun') + line_info.count('sat')
+    infos['num_business_days'] = len(line_info) - infos['num_weekend_days'] - 1
+
+    return infos
